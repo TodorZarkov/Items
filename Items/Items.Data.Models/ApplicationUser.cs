19cd2990052dd3998105df5344c43556cb6eafc2
TodorZarkov <t.p.zarkov@gmail.com>
2023-07-18 16:55:00 +0300

    ApplicationUser entity added

--- Items/Items.Data.Models/ApplicationUser.cs
+++ Items/Items.Data.Models/ApplicationUser.cs
@@ -4,5 +4,16 @@
 
     public class ApplicationUser : IdentityUser<Guid>
     {
+        public ApplicationUser() 
+        {
+            Items = new HashSet<Item>();
+            OwnCategories = new HashSet<Category>();
+            Locations = new HashSet<Location>();
+        }
+        public ICollection<Item> Items { get; set; }
+
+        public ICollection<Category> OwnCategories { get; set; }
+
+        public ICollection<Location> Locations { get; set; }
     }
 }

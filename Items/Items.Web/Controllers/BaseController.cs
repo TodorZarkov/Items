4b79d79ee961630b2f716a71c221b90b5ed861d3
TodorZarkov <t.p.zarkov@gmail.com>
2023-09-09 15:38:13 +0300

    fix places dropdown script, add anti csrf

--- Items/Items.Web/Controllers/BaseController.cs
+++ Items/Items.Web/Controllers/BaseController.cs
@@ -6,6 +6,7 @@
 	using Microsoft.AspNetCore.Mvc;
 
 	[Authorize]
+	[AutoValidateAntiforgeryToken]
 	public abstract class BaseController : Controller
 	{
 
--- Items/Items.Web/wwwroot/js/site.js
+++ Items/Items.Web/wwwroot/js/site.js
@@ -1,54 +1,62 @@
 ﻿
-window.onload = onLocationChange;
-const locationSelectEl = document.querySelector("#LocationId");
-if (locationSelectEl) {
-    locationSelectEl.onchange = onLocationChange
-}
-
-
-
-async function onLocationChange(event) {
-
-    const locationSelect = document.querySelector("#LocationId");
-    const locationId = locationSelect?.value;
-    const placeSelect = document.querySelector("#PlaceId");
-    const currentPlaceId = placeSelect?.value;
-
-    if (!locationId || !placeSelect) {
+function cascadePlacesDropdown(baseHost) {
+    if (!baseHost) {
         return;
     }
 
-
-    const placesResponce = await fetch("https://localhost:7121/Place/AllByLocation/" + locationId, {
-        credentials: "include",
-    });
-    if (placesResponce.status != 200) {
-        const option = createOption("", "Select Place.")
-        placeSelect.innerHTML = "";
-        placeSelect.append(option);
-        return;
+    window.onload = onLocationChange;
+    const locationSelectEl = document.querySelector("#LocationId");
+    if (locationSelectEl) {
+        locationSelectEl.onchange = onLocationChange
     }
-    const places = await placesResponce.json();
 
-    
 
-    if (event.type === "change") {
-        placeSelect.innerHTML = "";
-        options = places
-            .map(p => createOption(p.placeId, p.placeName));
-    } else {
-        options = places
-            .filter(p => p.placeId != currentPlaceId)
-            .map(p => createOption(p.placeId, p.placeName));
+
+    async function onLocationChange(event) {
+
+        const locationSelect = document.querySelector("#LocationId");
+        const locationId = locationSelect?.value;
+        const placeSelect = document.querySelector("#PlaceId");
+        const currentPlaceId = placeSelect?.value;
+
+        if (!locationId || !placeSelect) {
+            return;
+        }
+
+        //base host ssshttps://localhost:7121
+        const placesResponce = await fetch(baseHost + "/Place/AllByLocation/" + locationId, {
+            credentials: "include",
+        });
+        if (placesResponce.status != 200) {
+            const option = createOption("", "Select Place.")
+            placeSelect.innerHTML = "";
+            placeSelect.append(option);
+            return;
+        }
+        const places = await placesResponce.json();
+
+
+
+        if (event.type === "change") {
+            placeSelect.innerHTML = "";
+            options = places
+                .map(p => createOption(p.placeId, p.placeName));
+        } else {
+            options = places
+                .filter(p => p.placeId != currentPlaceId)
+                .map(p => createOption(p.placeId, p.placeName));
+        }
+        placeSelect.append(...options);
     }
-    placeSelect.append(...options);
+
+    function createOption(value, text) {
+        const option = document.createElement("option");
+        option.value = value;
+        option.innerText = text;
+
+        return option;
+    }
+
 }
 
-function createOption(value, text) {
-    const option = document.createElement("option");
-    option.value = value;
-    option.innerText = text;
-
-    return option;
-}
 

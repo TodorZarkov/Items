decb4accb4235ab4e89ca7852b24c1c9a9cf7067
TodorZarkov <t.p.zarkov@gmail.com>
2023-11-25 01:33:04 +0200

    added AllTicketServiceModel

---
+++ Items/Items.Services.Data.Models/Ticket/AllTicketServiceModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Items.Services.Data.Models.Ticket
+{
+	using static Items.Common.EntityValidationConstants;
+	using System.ComponentModel.DataAnnotations.Schema;
+	using System.ComponentModel.DataAnnotations;
+
+	public class AllTicketServiceModel
+	{
+		public Guid Id { get; set; }
+
+		
+		public string Type { get; set; } = null!;
+
+
+		public string Title { get; set; } = null!;
+
+
+		public string Status { get; set; } = null!;
+
+
+	}
+}

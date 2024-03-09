ac98686788f34d3de2a40c4c2ba7d7b7b3f94c21
TodorZarkov <t.p.zarkov@gmail.com>
2024-03-10 00:19:55 +0200

    Added CannotContainAttribute prevent deleting of the current image

--- Items/Items.Web.ViewModels/Item/ItemEditFormModel.cs
+++ Items/Items.Web.ViewModels/Item/ItemEditFormModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace Items.Web.ViewModels.Item
 {
+	using Items.Web.Validators.Attributes;
 	using Microsoft.AspNetCore.Http;
+	using System.ComponentModel.DataAnnotations;
 
 	public class ItemEditFormModel : ItemFormModel
 	{
@@ -10,10 +12,13 @@
             CurrentImages = new HashSet<Guid>();
             Images = new HashSet<IFormFile>();
         }
+
+        [Required]
         public Guid MainImageId { get; set; } //io
 
         public ICollection<Guid> CurrentImages { get; set; } //o
 
+        [CannotContain(nameof(MainImageId),"Cannot delete the current image!")]
         public ICollection<Guid> ImagesToDelete { get; set; } //i
 
         
---
+++ Items/Validators/Attributes/CannotContainAttribute.cs
@@ -0,0 +1,57 @@
+﻿namespace Items.Web.Validators.Attributes
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class CannotContainAttribute : ValidationAttribute 
+	{
+		private readonly string forbiddenGuidProperty;
+		public CannotContainAttribute(string forbiddenProperty, string errorMessage) : base(errorMessage)
+		{
+			this.forbiddenGuidProperty = forbiddenProperty;
+		}
+		 
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value == null)
+			{
+				return ValidationResult.Success;
+			}
+
+			object? property = validationContext.ObjectType
+				.GetProperty(forbiddenGuidProperty)?
+				.GetValue(validationContext.ObjectInstance);
+			if (property == null)
+			{
+				return ValidationResult.Success;
+			}
+
+			if (property.GetType() != typeof(Guid))
+			{
+				throw new ArgumentException("The Attribute accepts only properties of type Guid.");
+			}
+
+			Guid forbiddenValue = (Guid)property;
+
+			Type valueType = value.GetType();
+			if (valueType.IsAssignableTo(typeof(IEnumerable<Guid>)))
+			{
+				IEnumerable<Guid> valueCollection = (IEnumerable<Guid>)value;
+				if (valueCollection.Contains(forbiddenValue))
+				{
+					return new ValidationResult(this.ErrorMessage);
+				}
+			}
+			else if (valueType.IsInstanceOfType(typeof(Guid)))
+			{
+				Guid valueGuid = (Guid)value;
+				if (valueGuid == forbiddenValue)
+				{
+					return new ValidationResult(this.ErrorMessage);
+				}
+			}
+			return ValidationResult.Success;
+
+		}
+	}
+}

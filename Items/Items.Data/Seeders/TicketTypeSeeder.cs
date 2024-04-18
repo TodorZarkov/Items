64c3abfbb6fb0ace96d27a02aa0077c7e579acba
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-18 21:23:49 +0300

    Added TicketTypeSeeder

---
+++ Items/Items.Data/Seeders/TicketTypeSeeder.cs
@@ -0,0 +1,42 @@
+﻿namespace Items.Data.Seeders
+{
+    using Items.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using static Items.Common.TicketConstants.Types;
+
+    public static class TicketTypeSeeder
+    {
+        public static ModelBuilder SeedTicketTypes(this ModelBuilder builder)
+        {
+            builder
+                .Entity<TicketStatus>()
+                .HasData(GenerateTicketTypes());
+
+            return builder;
+        }
+
+        private static TicketType[] GenerateTicketTypes()
+        {
+            TicketType[] types = new[]
+            {
+                new TicketType
+                {
+                    Id = 1,
+                    Name = BUG
+                },
+                new TicketType
+                {
+                    Id = 2,
+                    Name = NEW_CATEGORY
+                },
+                new TicketType
+                {
+                    Id = 3,
+                    Name = NEW_CURRENCY
+                }
+            };
+
+            return types;
+        }
+    }
+}

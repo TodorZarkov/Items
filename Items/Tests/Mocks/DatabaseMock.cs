100317d1cea5b588b3bf7323675af94f99b97530
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-19 21:46:07 +0300

    Added Database Mock

---
+++ Items/Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.Mocks
+{
+    using Items.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    internal static class DatabaseMock
+    {
+        public static ItemsDbContext Instance 
+        {
+            get
+            {
+                DbContextOptions<ItemsDbContext> opt = new DbContextOptionsBuilder<ItemsDbContext>()
+                    .UseInMemoryDatabase($"ItemsInMemoryDB{DateTime.Now.Ticks}")
+                    .Options;
+
+                return new ItemsDbContext(options: opt, seed: false);
+            }
+        }
+    }
+}
---
+++ Items/Tests/UnitTests/UnitTestsBase.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests.UnitTests
+{
+    using AutoMapper;
+    using Items.Data;
+    using System;
+    using Tests.Mocks;
+
+    public class UnitTestsBase
+    {
+        protected ItemsDbContext dbContext;
+        protected IMapper mapper;
+
+        [OneTimeSetUp]
+        public void SetUpBase()
+        {
+            this.dbContext = DatabaseMock.Instance;
+            //this.mapper = MapperMock.Instance;
+
+            SeedDatabase();
+        }
+
+        private void SeedDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

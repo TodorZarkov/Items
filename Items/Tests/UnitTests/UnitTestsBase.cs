9a8c62eba471fd0a35f33ae3d8df5a934025e74d
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-19 22:12:28 +0300

    Added Base unit test

--- Items/Tests/UnitTests/UnitTestsBase.cs
+++ Items/Tests/UnitTests/UnitTestsBase.cs
@@ -2,6 +2,7 @@
 {
     using AutoMapper;
     using Items.Data;
+    using Items.Data.Models;
     using System;
     using Tests.Mocks;
 
@@ -18,10 +19,33 @@
 
             SeedDatabase();
         }
+        public ApplicationUser SuperAdmin { get; set; }
+
+        public ApplicationUser Category1Owner { get; set; }
+        public ApplicationUser Category23Owner { get; set; }
+
+        public ApplicationUser NoCategoryOwner { get; set; }
+
+        public Category Category1 { get; set; }
+
+        public Category Category2 { get; set; }
+
+        public Category Category3 { get; set; }
+
+        public Category AdminCategory1 { get; set; }
+
+        public Category AdminCategory2 { get; set; }
+
 
         private void SeedDatabase()
         {
             throw new NotImplementedException();
+        }
+
+        [OneTimeTearDown]
+        public void TearDownBase()
+        {
+            this.dbContext.Dispose();
         }
     }
 }

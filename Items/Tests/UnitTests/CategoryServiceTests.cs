90bb483e292a5c167a97e7acfcf17ceeea944931
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-21 17:31:53 +0300

    Tested GetMine and AllForSelect in category service

--- Items/Tests/UnitTests/CategoryServiceTests.cs
+++ Items/Tests/UnitTests/CategoryServiceTests.cs
@@ -18,7 +18,7 @@
 
 
         [Test]
-        public async Task GetAllPublic_ShouldReturnOnlyCategoriesCreatedByAdmins()
+        public async Task GetAllPublicAsync_ShouldReturnOnlyCategoriesCreatedByAdmins()
         {
             //Arrange: atleast one user with role admin or super admin must be arranged;
             //atleast one category which is created by user in role admin or supper admin 
@@ -29,6 +29,34 @@
                 await categoryService.GetAllPublicAsync();
 
             //Assert that a correct number of models and the correct ids are in it
+            Assert.That(adminCategories.Count, Is.EqualTo(3), "Checks the number of admin categories.");
+
+            //Assert that a correct ids are in the result
+            Assert.That(adminCategories.Select(ac => ac.Id).Intersect(new int[] { 6, 4, 5 }).Count(), Is.EqualTo(3), "Checks whether the expected category ids 4,5,6 are actually these.");
+        }
+
+        [Test]
+        public async Task GetMineAsync_ShouldReturnOnlyTheSpecifiedUserCategories()
+        {
+            var userCategories =
+                await categoryService.GetMineAsync(Category23Owner.Id);
+
+            Assert.That(userCategories.Count, Is.EqualTo(2)
+                , "Checks the number of the user categories.");
+            Assert.That(userCategories.Select(uc => uc.Id).Intersect(new int[] { 2, 3}).Count(), Is.EqualTo(2)
+                , "Checks whether the expected category ids 2,3 are actually these.");
+        }
+
+        [Test]
+        public async Task AllForSelectAsync_ShouldReturnAllTheSpecifiedUserCategoriesAndAllTheAdminCategories()
+        {
+            var forSelectCategories =
+                await categoryService.AllForSelectAsync(Category23Owner.Id);
+
+            Assert.That(forSelectCategories.Count, Is.EqualTo(5)
+                , "Checks the number of the user and admin's(public) categories.");
+            Assert.That(forSelectCategories.Select(c => c.Id).Intersect(new int[] { 2, 3, 6 , 4 , 5 }).Count(), Is.EqualTo(5)
+                , "Checks whether the expected category ids 2, 3, 6 , 4 , 5 are actually these.");
         }
 
     }

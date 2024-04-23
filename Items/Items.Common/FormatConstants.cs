b231f5fad8ad422e7908a4f9a453cf3c2122fda2
TodorZarkov <t.p.zarkov@gmail.com>
2024-04-24 02:22:38 +0300

    Fixed broken Auction End Date in Bid View

--- Items/Items.Common/FormatConstants.cs
+++ Items/Items.Common/FormatConstants.cs
@@ -4,7 +4,7 @@
 	{
 		public static class DateAndTime
 		{
-			public const string BiddingLongUtcDateTime = "MMM d yyyy H:mm:ss UTC";
+			public const string BiddingLongUtcDateTime = "MMM-d-yyyy-H:mm:ss-UTC";
 			public const string ContractDateTime = "MMM d yyyy";
 			public const string RotatedItemsDateTime = "MMM yyyy";
 		}

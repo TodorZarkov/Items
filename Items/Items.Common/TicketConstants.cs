28a8e36831d6aac09e0d27d2ccd12133fa97bc30
TodorZarkov <t.p.zarkov@gmail.com>
2024-07-06 17:13:39 +0300

    Added mapping statusesBE <--> statusesFE wit comments

--- Items/Items.Common/TicketConstants.cs
+++ Items/Items.Common/TicketConstants.cs
@@ -4,10 +4,10 @@
 	{
 		public static class Statuses
 		{
-			public const string OPEN = "Open";
-			public const string ASSIGN = "Assign";
-			public const string CLOSE = "Close";
-			public const string DELETE = "Delete";
+			public const string OPEN = "Open";//This corresponds to Rised/Reported
+			public const string ASSIGN = "Assign";//This corr. to opened/assigned
+			public const string CLOSE = "Close";//--> solved
+			public const string DELETE = "Delete";//not have relation with fe.
 		}
 
 		public static class Types
@@ -15,9 +15,10 @@
 			public const string BUG = "Bug";
 			public const string NEW_CATEGORY = "NewCategory";
 			public const string NEW_CURRENCY = "NewCurrency";
-		}
+            //public const string NEW_UNIT = "NewUnit";
+        }
 
-		public static class Query
+        public static class Query
 		{
 			public const int HitsPerPage = 20;
 			public const int CurrentPage = 1;
